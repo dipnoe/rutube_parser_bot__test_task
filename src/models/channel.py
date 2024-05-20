b185ec46ad186fb7f75f691a715e9b65f7a867f3
Elena Vagner <develop.vagner@gmail.com>
2024-05-20 16:39:47 +0700

    feat: update channel model; make migration

--- src/models/channel.py
+++ src/models/channel.py
@@ -8,10 +8,8 @@ class Channel(Base):
     __tablename__ = "channels"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String, nullable=False, unique=True)
-    description = Column(String, nullable=False)
+    title = Column(String, nullable=False, unique=True)
     channel_url = Column(String, nullable=False, unique=True)
-    followers_count = Column(Integer, nullable=False)
     videos = relationship("Video", back_populates="channel")
 
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
