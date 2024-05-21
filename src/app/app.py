ffeed9938ad334b11a899911b147b51cadac5934
Elena Vagner <develop.vagner@gmail.com>
2024-05-21 10:39:15 +0700

    feat: add app; update main

---
+++ src/app/app.py
@@ -0,0 +1,35 @@
+import asyncio
+import functools
+
+from bot.bot import Bot
+from config import settings, logger
+from config.db import get_db
+from repository.manager import RepositoryManager
+
+
+def retry_on_exception_async(max_retries=2, delay=1, exceptions=(Exception,)):
+    def decorator(func):
+        @functools.wraps(func)
+        async def wrapper(*args, **kwargs):
+            attempts = 0
+            while attempts < max_retries:
+                try:
+                    return await func(*args, **kwargs)
+                except exceptions as e:
+                    attempts += 1
+                    logger.error(f"Attempt {attempts} failed with exception: {e}. Retrying in {delay} seconds...")
+                    await asyncio.sleep(delay)
+            logger.fatal(f"All {max_retries} attempts failed.")
+            raise
+
+        return wrapper
+
+    return decorator
+
+
+@retry_on_exception_async(10, 5)
+async def run() -> None:
+    with get_db() as session:
+        repo = RepositoryManager(session)
+        bot = Bot(settings=settings, repo=repo)
+        await bot.start()
--- src/main.py
+++ src/main.py
@@ -1,35 +1,10 @@
 import asyncio
-import random
 
-from config.db import get_db
-from models.user import User
-from repository.manager import RepositoryManager
-from service.data_saver import ParsedDataSaver
-from service.parser import ChannelParser
-
-CHANNEL_URL = 'https://rutube.ru/u/goblin/'
+from app.app import run
 
 
 async def main():
-    with get_db() as session:
-        repo = RepositoryManager(session)
-
-        user = User(
-            id=3,
-            telegram_id=random.randint(1, 1000000)
-        )
-
-        channel = await ChannelParser(
-            channel_url=CHANNEL_URL,
-            parse_video_amount=1002
-        ).parse()
-
-        print(f'Name: {channel.title}')
-        print(f'Url: {channel.channel_url}')
-        print(f'Videos parsed: {len(channel.videos)}')
-
-        ParsedDataSaver(repo=repo).save_channel(user=user, channel_dto=channel)
-
+    await run()
 
 if __name__ == '__main__':
     asyncio.run(main())
