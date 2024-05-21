5a156e9ce1eaa4f22f9f13c4d197c0e3ee299edb
Elena Vagner <develop.vagner@gmail.com>
2024-05-21 07:24:47 +0700

    feat: update models with unique constraints; add migrations

--- src/models/channel.py
+++ src/models/channel.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import relationship
 
 from config.db import Base
@@ -6,11 +6,14 @@ from config.db import Base
 
 class Channel(Base):
     __tablename__ = "channels"
+    __table_args__ = (
+        UniqueConstraint('user_id', 'channel_url', name='uix_user_channel'),
+    )
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    title = Column(String, nullable=False, unique=True)
-    channel_url = Column(String, nullable=False, unique=True)
+    title = Column(String, nullable=False, unique=False)
+    channel_url = Column(String, nullable=False, unique=False)
     videos = relationship("Video", back_populates="channel")
 
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False, unique=False)
     user = relationship("User", back_populates="channels")
--- src/models/video.py
+++ src/models/video.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import relationship
 
 from config.db import Base
@@ -7,12 +7,15 @@ DESCRIPTION_MAX_LENGTH = 100
 
 class Video(Base):
     __tablename__ = "videos"
+    __table_args__ = (
+        UniqueConstraint('channel_id', 'video_url', name='uix_channel_video'),
+    )
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    title = Column(String, nullable=False)
-    description = Column(String(DESCRIPTION_MAX_LENGTH), nullable=False)
-    video_url = Column(String, nullable=False, unique=True)
-    views_count = Column(Integer, nullable=False)
+    title = Column(String, nullable=False, unique=False)
+    description = Column(String(DESCRIPTION_MAX_LENGTH), nullable=False, unique=False)
+    video_url = Column(String, nullable=False, unique=False)
+    views_count = Column(Integer, nullable=False, unique=False)
 
-    channel_id = Column(Integer, ForeignKey("channels.id"), nullable=False)
+    channel_id = Column(Integer, ForeignKey("channels.id"), nullable=False, unique=False)
     channel = relationship("Channel", back_populates="videos")
